97c83d776ec24f4da29f8bc4ec816268c8936657
EvgeniyBudaev <49036840+EvgeniyBudaev@users.noreply.github.com>
2021-06-02 20:15:49 +0300

    Правильная работа со статикой в шаблонах

--- products/views.py
+++ products/views.py
@@ -7,3 +7,33 @@ def index(request):
 
 def products(request):
     return render(request, 'products/products.html')
+
+
+def test_context(request):
+    context = {
+        'title': 'store',
+        'header': 'Добро пожаловать',
+        'username': 'Иван Иванов',
+        'products': [
+            {
+                'name': 'Худи черного цвета с монограммами adidas Originals',
+                'price': 6090.00
+            },
+            {
+                'name': 'Синяя куртка The North Face',
+                'price': 23725.00
+            },
+            {
+                'name': 'Коричневый спортивный oversized-топ ASOS DESIGN',
+                'price': 3390.00
+            },
+        ],
+        'promotion': True,
+        'products_of_promotion': [
+            {
+                'name': 'Белый спортивный костюм Nike',
+                'price': 15390.00
+            },
+        ]
+    }
+    return render(request, 'products/test-context.html', context)
