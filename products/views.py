5713478fac7b3665cd6257954b605e7a4f2a80fa
EvgeniyBudaev <49036840+EvgeniyBudaev@users.noreply.github.com>
2021-06-04 04:31:22 +0300

    Медиафайлы

--- products/views.py
+++ products/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from products.models import ProductCategory, Product
+
 
 def index(request):
     context = {
@@ -10,36 +12,9 @@ def index(request):
 
 def products(request):
     context = {
-        'title': 'Store - Каталог'
+        'title': 'Store - Каталог',
+        'categories': ProductCategory.objects.all(),
+        'products': Product.objects.all(),
     }
     return render(request, 'products/products.html', context)
 
-
-def test_context(request):
-    context = {
-        'title': 'store',
-        'header': 'Добро пожаловать',
-        'username': 'Иван Иванов',
-        'products': [
-            {
-                'name': 'Худи черного цвета с монограммами adidas Originals',
-                'price': 6090.00
-            },
-            {
-                'name': 'Синяя куртка The North Face',
-                'price': 23725.00
-            },
-            {
-                'name': 'Коричневый спортивный oversized-топ ASOS DESIGN',
-                'price': 3390.00
-            },
-        ],
-        'promotion': True,
-        'products_of_promotion': [
-            {
-                'name': 'Белый спортивный костюм Nike',
-                'price': 15390.00
-            },
-        ]
-    }
-    return render(request, 'products/test-context.html', context)
