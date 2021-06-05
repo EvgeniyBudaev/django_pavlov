788aa03ddd5d8f9b0501f67a0eb4fe95a8307a4d
EvgeniyBudaev <49036840+EvgeniyBudaev@users.noreply.github.com>
2021-06-05 07:10:22 +0300

    Декораторы

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse
 from django.contrib import auth, messages
+from django.contrib.auth.decorators import login_required
 
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 from products.models import Basket
@@ -37,6 +38,7 @@ def register(request):
     return render(request, 'users/register.html', context)
 
 
+@login_required
 def profile(request):
     user = request.user
     if request.method == 'POST':
@@ -63,7 +65,7 @@ def profile(request):
         'total_quantity': total_quantity,
         'total_sum': total_sum
     }
-    
+
     return render(request, 'users/profile.html', context)
 
 
