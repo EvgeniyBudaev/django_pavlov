ac9864c123f31e0751b9e50fc2f704ebde37d67c
EvgeniyBudaev <49036840+EvgeniyBudaev@users.noreply.github.com>
2021-06-05 06:22:02 +0300

    Методы корзины

--- users/views.py
+++ users/views.py
@@ -47,10 +47,23 @@ def profile(request):
             return HttpResponseRedirect(reverse('users:profile'))
     else:
         form = UserProfileForm(instance=user)
+
+    baskets = Basket.objects.filter(user=user)
+    total_quantity = sum(basket.quantity for basket in baskets)
+    total_sum = sum(basket.sum() for basket in baskets)
+    # total_quantity = 0
+    # total_sum = 0
+    # for basket in baskets:
+    #     total_quantity += basket.quantity
+    #     total_sum += basket.sum()
+
     context = {
         'form': form,
-        'baskets': Basket.objects.filter(user=user)
+        'baskets': baskets,
+        'total_quantity': total_quantity,
+        'total_sum': total_sum
     }
+    
     return render(request, 'users/profile.html', context)
 
 
